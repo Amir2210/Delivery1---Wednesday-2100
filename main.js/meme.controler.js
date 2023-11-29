99fd08f3b5c945ce9e9c937cb6206152fd709acd
Amir Yankolovich <amiryankolovich2000@gmail.com>
2023-11-29 13:37:50 +0200

    Add drawLineText function

--- main.js/meme.controler.js
+++ main.js/meme.controler.js
@@ -11,6 +11,18 @@ function onInit() {
 
 function renderMeme(elImg) {
   gCtx.drawImage(elImg, 0, 0, gCanvas.width, gCanvas.height)
+  drawLineText("your text...", 225, 50)
+}
+
+function drawLineText(text, x, y) {
+  gCtx.lineWidth = 3.5
+  gCtx.strokeStyle = "#000"
+  gCtx.fillStyle = "#fff"
+  gCtx.font = "60px Impact"
+  gCtx.textAlign = "center"
+  gCtx.textBaseline = "middle"
+  gCtx.fillText(text, x, y)
+  gCtx.strokeText(text, x, y)
 }
 
 // function onDownloadCanvas(elLink) {
