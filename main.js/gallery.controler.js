9eff5429ae160bbe11404cca2b72fc836e2c4e7a
Amir Yankolovich <amiryankolovich2000@gmail.com>
2023-12-02 15:09:15 +0200

    Add filter functinality

--- main.js/gallery.controler.js
+++ main.js/gallery.controler.js
@@ -1,30 +1,62 @@
 "use strict"
 
 function renderImgs() {
+  const filterInput = document.getElementById("imageFilterInput")
+
+  // Get all images
+  const allImages = [...getSquareImgs()]
+
+  renderGallery(allImages)
+
+  filterInput.addEventListener("input", filterImages)
+}
+
+function filterImagesByKeyword(keyword) {
+  const allImages = [...getSquareImgs()]
+
+  const filteredImages = allImages.filter((img) =>
+    img.keywords.map((kw) => kw.toLowerCase()).includes(keyword.toLowerCase())
+  )
+
+  renderGallery(filteredImages)
+}
+
+function renderGallery(images) {
   const elImgsSection = document.querySelector(".imges")
-  var strHtml = getSquareImgs()
+  const strHtml = images
     .map(
       (img) =>
         `
-        <div class=" image-container">
-      <img data-id = ${img.id} onclick=" renderMeme(this)" src="images/meme-imgs (square)/${img.id}.jpg" alt="memeImg">
-      </div>
-      `
-    )
-    .join("")
-  strHtml += getAspectRatiosImgs()
-    .map(
-      (img) =>
-        `
-        <div class=" image-container">
-      <img data-id = ${img.id}  onclick=" renderMeme(this)" src="images/meme-imgs (various aspect ratios)/${img.id}.jpg" alt="memeImg">
-      </div>
-      `
+            <div class="image-container">
+                <img data-id=${img.id} onclick="renderMeme(this)" src="${img.src}" alt="memeImg">
+            </div>
+            `
     )
     .join("")
 
   elImgsSection.innerHTML = strHtml
 }
+
+function filterImages() {
+  const filterInput = document.getElementById("imageFilterInput")
+  const filterValue = filterInput.value.toLowerCase()
+
+  const allImages = [...getSquareImgs()]
+
+  const filteredImages = allImages.filter((img) => {
+    const keywords = img.keywords.map((keyword) => keyword.toLowerCase())
+    return keywords.some((keyword) => keyword.includes(filterValue))
+  })
+
+  renderGallery(filteredImages)
+}
+
+function clearFilter() {
+  const filterInput = document.getElementById("imageFilterInput")
+  filterInput.value = ""
+  renderImgs()
+}
+
 function goGallery() {
   const elSectionImg = document.querySelector(".gallery .imges")
   elSectionImg.classList.remove("hidden")
@@ -34,4 +66,6 @@ function goGallery() {
 
   const elMemeController = document.querySelector(".meme-controller")
   elMemeController.classList.add("hidden")
+
+  renderImgs()
 }
